432914364d7e91f819e536cd177e48e3442fab11
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2013-04-12 14:38:37 +0200

    refactor getting hs_serv value

--- src/app.js
+++ src/app.js
@@ -26,27 +26,35 @@ controller.index = function(req, res) {
     res.json(settings);
 }
 
+function get_server(prefix, callback) {
+    var url = "http://hdl.handle.net/api/0.NA/" + prefix + "?type=HS_SERV";
+    request(url, function(error, response, body) {
+        if (error || response.statusCode != 200) {
+            callback('prefix does not exist');
+        } else {
+            var json = JSON.parse(body);
+            var hs_serv = json.values[0].data;
+            callback(null, hs_serv);
+        }
+    })
+}
+
 controller.prefix = function(req, res) {
     var prefix = req.params.prefix;
     if (!prefix.match(/^10\..+/))
         res.send(404, 'not a doi prefix')
-    else {
-        var url = "http://hdl.handle.net/api/0.NA/" + prefix + "?type=HS_SERV";
-        request(url, function(error, response, body) {
-            if (error || response.statusCode != 200) {
-                res.send(404, 'prefix does not exist');
-            } else {
-                var json = JSON.parse(body);
-                var hs_serv = json.values[0].data;
-
-                var ra = servers[hs_serv];
+    else 
+        get_server(prefix, function(error, server) {
+            if (error) 
+                res.send(404, 'prefix not found')
+            else {
+                var ra = servers[server];
                 if (ra == undefined) 
-                res.send(404, 'no registration agency known for this prefix')
+                    res.send(404, 'no registration agency known for this prefix')
                 else 
                     res.redirect("/ra/" + ra);
             }
-        });
-    }
+        })
 }
 
 controller.ra = function(req, res) {
